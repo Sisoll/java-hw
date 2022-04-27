a4d4c5fb1d631cb46ebe28ba472b9d43c85a3cb9
Sisol Shie <sisolshie@gmail.com>
2022-04-27 20:14:27 +0800

    修改變數的宣告為var 增加防呆:如果輸入為負數/小數

--- src/ch04/Hw01.java
+++ src/ch04/Hw01.java
@@ -1,15 +1,12 @@
 package ch04;
 
 import java.util.Scanner;
-
 /**
  * 
  * @author SisolShie 1.請設計一程式，可讀入一正整數n，並印出1~n之內，3的倍數和4的倍數的總和
- *
- *
- *         防呆,變數首字母小寫 重構 junit 單元測試(針對class) (自動化) Kent Back
  * 
- * 
+ * 建立防呆,將一開始的輸入先當成字串s,將字串s先轉乘double s,然後強制轉型成int s,然後將double s 和ints 相減       
+ *
  */
 
 public class Hw01
@@ -19,44 +16,47 @@ public class Hw01
 	{
 		System.out.println("請輸入一個正整數");
 		var scanner = new Scanner(System.in);
-		int numInput = scanner.nextInt();
+		var input_s = scanner.nextLine();  //這邊先把輸入當成字串
 		scanner.close();
 
-		int sum0 = 0;
-		for (int i = 1; i <= numInput; i++)
+		
+		var input_d = Double.parseDouble(input_s);
+		
+		// 判斷輸入的是整數還是小數
+		boolean isINT = true;
+		if (input_d - ((int)input_d) > 0)
 		{
-			if ((i % 3 == 0 && i % 4 != 0) 
-					|| (i % 3 != 0 && i % 4 == 0) 
-					|| (i % 3 == 0 && i % 4 == 0))
+			isINT = false;
+		}		
+		
+		
+		if(isINT)
+		{
+			var input_i = (int)input_d ;
+			
+			if (input_i > 0)
 			{
-				sum0 = sum0 + i;
+				var sum = 0;
+				for (var i = 1; i <= input_i; i++)
+				{
+					if ((i % 3 == 0 && i % 4 != 0) 
+							|| (i % 3 != 0 && i % 4 == 0) 
+							|| (i % 3 == 0 && i % 4 == 0))
+					{
+						sum += i;
+					}
+				}
+				System.out.println("在正整數 1 到 正整數" + input_i + " 之間 , 3的倍數和4的倍數的總和為 " + sum);
+			}
+			else 
+			{
+				System.out.println("你輸入的數字為負數"); 	
 			}
 		}
-
-		/*
-		 * 另外一種比較沒效率的寫法 int sum3 = 0; int sum4 = 0; int sum12 = 0;
-		 * 
-		 * String all3 = ""; String all4 = "";
-		 * 
-		 * 
-		 * for (int i=0 ; i<= numInput ; i++) { if (i%3!=0) { continue; } sum3 = sum3+i;
-		 * all3 = all3 + i + " "; }
-		 * 
-		 * for (int i=0 ; i<= numInput ; i++) { if (i%4!=0) { continue; } sum4 = sum4+i;
-		 * all4 = all4 + i + " ";
-		 * 
-		 * }
-		 * 
-		 * for (int i=0 ; i<= numInput ; i++) { if (i%12==0) { sum12 = sum12+i; } }
-		 * 
-		 * int sum = sum3 + sum4 - sum12; //減去3和4的公倍數
-		 * 
-		 * 
-		 * System.out.println("3的倍數有" + all3); System.out.println("4的倍數有" + all4);
-		 * 
-		 * System.out.println("在 1 到 " + numInput +" 之間 , 3的倍數和4的倍數的總和為 " + sum);
-		 * 
-		 */
-		System.out.println("在 1 到 " + numInput + " 之間 , 3的倍數和4的倍數的總和為 " + sum0);
+		else 
+		{
+			System.out.println("你輸入的數字為小數"); 	
+		}
+    	
 	}
 }
