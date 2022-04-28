d349a12ee092bddb7b8e10b51e0fcb3d23be7666
Bo-Xuan Fan <coffee0127@gmail.com>
2022-04-29 00:08:03 +0800

    Review Hw04

--- src/ch05/Hw04.java
+++ src/ch05/Hw04.java
@@ -3,7 +3,7 @@ package ch05;
 import java.util.Arrays;
 
 /**
- * 
+ *
  * @author SisolShie
  * 4.有一字串陣列內容如下…
  * String[] mySkills = {"java", "javascript", "sql", "c", "python", "php", "swift"};
@@ -16,11 +16,12 @@ public class Hw04
 	public static void main (String[] args)
 	{
 		 String[] mySkills = {"java", "javascript", "sql", "c", "python", "php", "swift"};
-		 
+
 		 //System.out.println(mySkills[0]);
 	 	  System.out.println(Arrays.toString(mySkills));
-	 	 
 
+
+         // Tips: 用 Array & char 'a' 去計算
 	 	 int a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,i=0,j=0,k=0,l=0,m=0,n=0,o=0,p=0,q=0,r=0,s=0,t=0,u=0,v=0,w=0,x=0,y=0,z=0;
 	 	 for (int ii=0 ; ii<mySkills.length ; ii++)
 	 	 {
@@ -31,73 +32,77 @@ public class Hw04
 	 		 for (int jj = 0 ; jj < mySkills[ii].length() ; jj++)
 	 		 {
 	 		//	 System.out.print(xx[jj]);
-	 			 
-	 			 switch ( xx[jj]) 
+
+	 			 switch ( xx[jj])
 	 			 {
 	 				 case 'a' : a++;
 	 				 	break;
 	 				 case 'b' : b++;
-	 				 	break;	 
+	 				 	break;
 	 				 case 'c' : c++;
-	 				 	break;	 
+	 				 	break;
 	 				 case 'd' : d++;
-	 				 	break;	
+	 				 	break;
 	 				 case 'e' : e++;
-	 				 	break;	
+	 				 	break;
 	 				 case 'f' : f++;
 	 				 	break;
 	 				 case 'g' : g++;
-	 				 	break;	 
+	 				 	break;
 	 				 case 'h' : h++;
-	 				 	break;	 
+	 				 	break;
 	 				 case 'i' : i++;
-	 				 	break;	
+	 				 	break;
 	 				 case 'j' : j++;
-	 				 	break;	
+	 				 	break;
 	 				 case 'k' : k++;
 	 				 	break;
 	 				 case 'l' : l++;
-	 				 	break;	 
+	 				 	break;
 	 				 case 'm' : m++;
-	 				 	break;	 
+	 				 	break;
 	 				 case 'n' : n++;
-	 				 	break;	
+	 				 	break;
 	 				 case 'o' : o++;
-	 				 	break;	
+	 				 	break;
 	 				 case 'p' : p++;
 	 				 	break;
+	 				 // is `q++`, not `g++`
 	 				 case 'q' : g++;
-	 				 	break;	 
+	 				 	break;
 	 				 case 'r' : r++;
-	 				 	break;	 
+	 				 	break;
 	 				 case 's' : s++;
-	 				 	break;	
+	 				 	break;
 	 				 case 't' : t++;
 	 				 	break;
 	 				 case 'u' : u++;
-	 				 	break;	 
+	 				 	break;
 	 				 case 'v' : v++;
-	 				 	break;	
+	 				 	break;
 	 				 case 'w' : w++;
-	 				 	break;	
+	 				 	break;
 	 				 case 'x' : x++;
 	 				 	break;
 	 				 case 'y' : y++;
-	 				 	break;	 
+	 				 	break;
 	 				 case 'z' : z++;
-	 				 	break;	 
-	 				 default :  ;		 
+	 				 	break;
+	 				 // ; 是可以省略
+	 				 default :  ;
 	 			 }
-		 		 
+
 	 		 }
+             // print empty string?
 	 		 System.out.println("");
 	 	 }
-	 	 
+
+         // 為什麼要 ++ ?
 	 	 System.out.println("a:" + a++);
 	 	 System.out.println("s:" + s++);
 
-	
+
 	}
-	
-	
+
+
 }
