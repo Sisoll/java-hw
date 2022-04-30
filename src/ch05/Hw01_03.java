46e2a654aa17323ff6d97dfd254283594b66e50b
Sisol Shie <sisolshie@gmail.com>
2022-04-30 23:16:56 +0800

    上傳至雲端作業版本

--- src/ch05/Hw01_03.java
+++ src/ch05/Hw01_03.java
@@ -17,65 +17,64 @@ public class Hw01_03
 	public static void main(String[] args)
 	{
 		// HW1
+		System.out.println("HW1");
 		int numarray [] = new int [10];
 		
-		for (int i = 0 ; i < numarray.length ; i++)
-		{
-			numarray[i] = (int)((Math.random()*99+1)) ; 			
-			//System.out.println("第" + i + "個位置  第一次產生的陣列內容:"+numarray[i]);
+		for (var i = 0 ; i < numarray.length ; i++){
+			numarray[i] = (int)((Math.random()*101)) ; 			
+				//System.out.println("第" + i + "個位置  第一次產生的陣列內容:"+numarray[i]);
 			
 			back:			
-			for (int j=0 ; j < i ; j++)
-			{
-				//System.out.println("現在新的數字是" + numarray[i] + "   要比較的之前數字有 "+numarray[j]);
-				while (numarray[j] == numarray[i])
-				{
-					numarray[i] = (int)((Math.random()*99+1));
-					//System.out.println ("第" + i +"個位置   新陣列內容" + numarray[i]);
+			for (var j=0 ; j < i ; j++)	{
+					//System.out.println("現在新的數字是" + numarray[i] + "   要比較的之前數字有 "+numarray[j]);
+				while (numarray[j] == numarray[i])	{
+					numarray[i] = (int)((Math.random()*101));
+						//System.out.println ("第" + i +"個位置   新陣列內容" + numarray[i]);
 					j = -1 ; //很重要,若是不重新給初始值,continue之後,j會繼續往下跑,接著去比較i後面的數字
 					continue back;	
 				}
-				
 			}
 		}
 
 		System.out.print ( "陣列為 : ");
-		for (int i : numarray)
-		{
-			System.out.print (i + " " );
+		
+		for (var value : numarray){
+			System.out.print (value + " " );
 		}
-		System.out.println ( " ");
+		
+		System.out.println("");
 
 		
 		// HW2
-		int sum = 0;
-		for (int i=0 ; i<numarray.length ; i++)
-		{
+		System.out.println("HW2");
+
+		var sum = 0.0;
+		for (var i=0 ; i<numarray.length ; i++)	{
 			sum = sum +  numarray[i];
 		}
 
-		double avg = (double)sum / (double)numarray.length;
+		double avg = sum / numarray.length;
 		
-		System.out.println("陣列內數值的總和為: " + sum + "; 其平均數為: " + avg);
-
+		System.out.println("陣列內數值的總和為: " + (int)sum + "; 其平均數為: " + avg);
 		
 		//HW3
-		
+		System.out.println("HW3");
+
 		Arrays.sort(numarray);  //由小到大排序
 		System.out.println("排序後的陣列為: " + Arrays.toString(numarray));
 		
-		for (int i=0 ; i<numarray.length ; i++)
+		for (var i=0 ; i<numarray.length ; i++)
 		{
-			if ( (double)numarray[i] == avg)
+			if ( numarray[i] == avg)
 			{
-				System.out.println("陣列中索引 " + i + " 位置的值和平均數 " + (int)avg + " 相同" );
+				System.out.println("陣列中索引 " + i + " 位置的值和整個陣列的平均數 " + (int)avg + " 相同" );
 				break;
 			}
 			else if (numarray[i]>avg)
 			{
 				int minmaxI = i ;
 				int maxminI = i - 1 ; 
-				System.out.println("陣列述職的平均數 " + avg + " 位於索引為 " + maxminI + " 以及 " + minmaxI + " 之間");
+				System.out.println("陣列整個數值的平均數為 " + avg + " ,其位於索引 " + maxminI + " 以及 " + minmaxI + " 之間");
 				break;
 			}
 		}
