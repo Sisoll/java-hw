7c23692e76f6e50e2ac5f5f5c177f552fb54f938
Sisol Shie <sisolshie@gmail.com>
2022-04-30 23:21:26 +0800

    重新排版,上傳至雲端版本

--- src/ch04/Hw01.java
+++ src/ch04/Hw01.java
@@ -1,62 +1,50 @@
 package ch04;
 
 import java.util.Scanner;
+
 /**
  * 
  * @author SisolShie 1.請設計一程式，可讀入一正整數n，並印出1~n之內，3的倍數和4的倍數的總和
- * 
- * 建立防呆,將一開始的輸入先當成字串s,將字串s先轉乘double s,然後強制轉型成int s,然後將double s 和ints 相減       
+ *
+ *         建立防呆 (如果亂輸入ex.0.5,-5), 將一開始的輸入先當成double s,然後強制轉型成int去判斷是否相等
  *
  */
 
-public class Hw01
+public class Hw01 
 {
-
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
 		System.out.println("請輸入一個正整數");
 		var scanner = new Scanner(System.in);
-		var input_s = scanner.nextLine();  //這邊先把輸入當成字串
+		var input_d = scanner.nextDouble(); // 這邊先把輸入當成小數型態
 		scanner.close();
 
-		
-		var input_d = Double.parseDouble(input_s);
-		
+		// System.err.println( "hello");
+
 		// 判斷輸入的是整數還是小數
-		boolean isINT = true;
-		if (input_d - ((int)input_d) > 0)
-		{
-			isINT = false;
-		}		
 		
-		
-		if(isINT)
-		{
-			var input_i = (int)input_d ;
-			
-			if (input_i > 0)
-			{
+		if (input_d == (int) input_d) {
+			if (input_d > 0) {
 				var sum = 0;
-				for (var i = 1; i <= input_i; i++)
-				{
+				for (var i = 1; i <= input_d; i++) {
 					if ((i % 3 == 0 && i % 4 != 0) 
 							|| (i % 3 != 0 && i % 4 == 0) 
-							|| (i % 3 == 0 && i % 4 == 0))
-					{
+							|| (i % 3 == 0 && i % 4 == 0)) {
 						sum += i;
 					}
 				}
-				System.out.println("在正整數 1 到 正整數" + input_i + " 之間 , 3的倍數和4的倍數的總和為 " + sum);
+				System.out.println("在正整數 1 到 正整數 " + (int) input_d + " 之間 , 3的倍數和4的倍數的總和為 " + sum);
+			} 
+			else if (input_d == 0) {
+				System.out.println("你輸入的數字為 0");
+			} 
+			else {
+				System.out.println("你輸入的數字為負數");
 			}
-			else 
-			{
-				System.out.println("你輸入的數字為負數"); 	
-			}
+		} 
+		else {
+			System.out.println("你輸入的數字為小數");
 		}
-		else 
-		{
-			System.out.println("你輸入的數字為小數"); 	
-		}
-    	
+
 	}
 }
