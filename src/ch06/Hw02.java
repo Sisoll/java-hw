76b3a4ee26f3a894ffc99445b48740267cea899f
Sisol Shie <sisolshie@gmail.com>
2022-05-01 21:48:33 +0800

    1. 將原本ansToString內的if改為switch case 2. 原本寫法的ansToString方法不需要回傳值,改為有回傳值到main裡面,然後在main裡面print出來

--- src/ch06/Hw02.java
+++ src/ch06/Hw02.java
@@ -7,7 +7,7 @@ package ch06;
  *         add()、sub()、mul()、div()，表示分別加、減、乘、除，各方法都接受2個int型態的參數，並回傳對應的結果
  *      
  *         remark:	 Integer.toString(int , 16)  vs Integer.toHexString(int)
- *      						         signed  vs unsigned (沒有正負)
+ *      						         signed  vs unsigned (沒有正負,10進位中其值和signed相加為2^n , for int,n=32)
  */
 public class Hw02 {
 
@@ -15,33 +15,33 @@ public class Hw02 {
 		
 		Calculator cal = new Calculator();
 		
-		cal.radix = 16;
+		cal.radix = 2;
 		
-		cal.add(5,5); 
-		cal.sub(5,5);
-		cal.mul(5,5);
-		cal.div(5,5);
+		var result = cal.add(5,4); 
+		//cal.add(5,4); 
+		//cal.sub(5,5);
+		//cal.mul(5,5);
+		//cal.div(5,5);					
+		System.out.println(result);
+	
 	}
 
 	static class Calculator {
 
-		int radix ;   // 不給初始值,會當成一個0,如果main裡面沒給值,所以就會跑else,也就是預設為10進位的程式敘述
+		int radix ;  //會被當成0
 
 		private String ansToString(int ans) {
 			String result;
-
-			if (radix == 2) {
-				result = Integer.toBinaryString(ans); // Integer類別
-				System.out.println("使用 2 進位的答案輸出為: " + result);
-			} else if (radix == 8) {
-				result = Integer.toOctalString(ans);
-				System.out.println("使用 8 進位的答案輸出為: " + result);
-			} else if (radix == 16) {
-				result = Integer.toHexString(ans);
-				System.out.println("使用 16 進位的答案輸出為: " + result);
-			} else {
-				result = Integer.toString(ans);
-				System.out.println("使用 10 進位(預設)的答案輸出為: " + result);
+			
+			switch (radix) {
+			case  2: result = "使用 2 進位的答案輸出為: " + Integer.toBinaryString(ans); // Integer類別
+					break;
+			case  8: result = "使用 8 進位的答案輸出為: " + Integer.toOctalString(ans);
+					break;
+			case 16: result = "使用 16 進位的答案輸出為: " + Integer.toHexString(ans);
+					break;
+			default :result = "使用 10 進位(預設)的答案輸出為: " + Integer.toString(ans);
+					break;
 			}
 			return result;
 		}
