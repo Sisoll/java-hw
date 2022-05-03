d1edf523fde0021682224fc70425822c7e474570
Sisol Shie <sisolshie@gmail.com>
2022-05-04 00:26:12 +0800

    1.建構子初始化問題: solved 2.Scanner : 問題在System.in不能多次關閉,目前先把close()刪除後正常.
    3.重新排版,新增解說.

--- src/ch06/ScoreCalculator.java
+++ src/ch06/ScoreCalculator.java
@@ -3,46 +3,64 @@ package ch06;
 import java.util.Arrays;
 import java.util.Scanner;
 
-// TODO     MAX MIN AVG 不能同時做!?????????
-// ???????  為什麼ScoreCalculator的studentLimit不能直接抓建構子內設定的值?
-
+/**
+ * 
+ * 10.請定義一個成績計算器類別ScoreCalculator..
+ * 
+ * 2個private欄位: studentArray(學生陣列)、studentCount(已加入學生人數) 2個public建構子 無參數建構子:
+ * 初始化學生人數上限為5 1個參數建構子: 傳入一int值studentLimit，表示學生人數上限 多個public方法 addStudent(學生):
+ * 可加入一學生物件 getMaxScoreStudent(科目): 回傳參數所指定科目的最高分學生 getMinScoreStudent(科目):
+ * 回傳參數所指定科目的最低分學生 getAvgScore(科目): 回傳參數所指定科目的平均分數
+ *
+ * remark : 在close scanner的時候也會把system.in給關掉,這時候再去run他會有問題 還想不到在哪裡關所以先不關....
+ * 
+ * 
+ * @author SisolShie
+ * 
+ *         //TODO 科目輸入的防呆
+ */
 public class ScoreCalculator {
 	public static void main(String[] args) {
 
-		//1.利用Student建立學生物件 st1 / st2 / st3 / st4 /st5
-		Student st1 = new Student(1, "T", 50, 80, 60);
-		Student st2 = new Student(2, "S", 66, 85, 93);
-		Student st3 = new Student(3, "Q", 45, 68, 25);
-		Student st4 = new Student(4, "A", 39, 62, 55);
-		Student st5 = new Student(5, "H", 26, 90, 20);
+		// 1.利用Student建立學生物件 st1 / st2 / st3 / st4 /st5
+		Student st1 = new Student(1, "A", 50, 80, 60);
+		Student st2 = new Student(2, "B", 66, 85, 93);
+		Student st3 = new Student(3, "C", 45, 68, 25);
+		Student st4 = new Student(4, "D", 39, 62, 55);
+		Student st5 = new Student(5, "E", 26, 90, 20);
+		Student st6 = new Student(6, "F", 96, 77, 33);
 
-		//2.建立ScoreCalculator物件 
-		ScoreCalculator s = new ScoreCalculator();
+		// 2.建立ScoreCalculator物件
+		ScoreCalculator s = new ScoreCalculator(6); // 不給值的話上限只有5,輸入6以上的話需要給值才不會錯
 
-		//3.將學生成績加入studentArray
-		s.setStudentArray(s.addStudent(st1, s.getStudentCount()), s.getStudentCount()); //
-		s.setStudentArray(s.addStudent(st2, s.getStudentCount()), s.getStudentCount()); //
-		s.setStudentArray(s.addStudent(st3, s.getStudentCount()), s.getStudentCount()); //
-		s.setStudentArray(s.addStudent(st4, s.getStudentCount()), s.getStudentCount()); //
-		s.setStudentArray(s.addStudent(st5, s.getStudentCount()), s.getStudentCount()); //
-			// System.out.println(Arrays.deepToString(s.getStudentArray()));
-		
-		
-		//最高分/最低分/平均值
-		// System.out.println(s.getMaxScoreStudent());
-		// System.out.println(s.getMinScoreStudent());
+		// 3.將學生成績加入studentArray
+		// 呼叫 setStudentArray 中 輸入兩個參數(陣列 , 位置)的方法
+		// 其中陣列的產生是呼叫 addStudent 中 需要 (學生物件 , 位置) 的方法
+		s.setStudentArray(s.addStudent(st1, s.getStudentCount()), s.getStudentCount());
+		s.setStudentArray(s.addStudent(st2, s.getStudentCount()), s.getStudentCount());
+		s.setStudentArray(s.addStudent(st3, s.getStudentCount()), s.getStudentCount());
+		s.setStudentArray(s.addStudent(st4, s.getStudentCount()), s.getStudentCount());
+		s.setStudentArray(s.addStudent(st5, s.getStudentCount()), s.getStudentCount());
+		s.setStudentArray(s.addStudent(st6, s.getStudentCount()), s.getStudentCount());
+
+		// 最高分/最低分/平均值
+		System.out.println(s.getMaxScoreStudent());
+		System.out.println(s.getMinScoreStudent());
 		System.out.println(s.getAvgScore());
+
 	}
 
-	int aaaaa;
+	// field -------------------------
+
 	private int[][] studentArray;
 	private int studentCount;
 	String[] nameList;
-	int studentLimit = 5;  
+	int studentLimit;
 
-	// 建構子
+	// 建構子 -------------------------------------
 	ScoreCalculator() {
 		int studentLimit = 5;
+		this.studentLimit = studentLimit;
 		setStudentArray(studentLimit);
 		setStudentCount(0);
 		nameList = new String[studentLimit];
@@ -52,16 +70,18 @@ public class ScoreCalculator {
 		this.studentLimit = studentLimit;
 		setStudentArray(studentLimit);
 		setStudentCount(0);
+		nameList = new String[studentLimit];
 	}
 
-	
-	//  SETTER / GETTER  --------------------------------
+	// SETTER / GETTER --------------------------------
 	public int[][] getStudentArray() {
 		return studentArray;
 	}
 
 	public void setStudentArray(int studentLimit) {
+		
 		int[][] studentArray = new int[studentLimit][5];
+		
 		for (var i = 0; i < studentLimit; i++) {
 			for (var j = 0; j < 5; j++) {
 				studentArray[i][j] = 0;
@@ -71,8 +91,10 @@ public class ScoreCalculator {
 	}
 
 	public void setStudentArray() {
+		
 		var studentLimit = 5;
 		int[][] studentArray = new int[studentLimit][5];
+		
 		for (var i = 0; i < studentLimit; i++) {
 			for (var j = 0; j < 5; j++) {
 				studentArray[i][j] = 0;
@@ -100,11 +122,11 @@ public class ScoreCalculator {
 	}
 
 	// METHOD -------------------------------------------
-	
-	//增加學生
-	public int[] addStudent(Student student, int studentCount) {
-		this.setStudentCount(studentCount);
 
+	// 增加學生
+	public int[] addStudent(Student student, int studentCount) {
+
+		this.setStudentCount(studentCount);
 		int no, chi, eng, math;
 		String name;
 		no = student.getNo();
@@ -113,48 +135,41 @@ public class ScoreCalculator {
 		eng = student.getEng();
 		math = student.getMath();
 		int[] arr = new int[] { studentCount, no, chi, eng, math };
-
 		int order = this.getStudentCount();
 		nameList[order] = name;
-		// System.out.println(studentCount + " "+ name + order);
-
+		
 		return arr;
 	}
 
-	//判斷科目
-	public int selectObject() {
+	// 判斷科目
+	public int selectSubject() {
 		System.out.println("請輸入科目:(chi / eng / math)");
 		Scanner scanner = new Scanner(System.in);
 		String obj = scanner.nextLine();
-		scanner.close();
 
 		int i = -1;
 		switch (obj) {
-		case "chi":
-			i = 2;
+		case "chi": i = 2;
 			break;
-		case "eng":
-			i = 3;
+		case "eng":	i = 3;
 			break;
-		case "math":
-			i = 4;
+		case "math": i = 4;
 			break;
 		default:
 			break;
 		}
+		
 		return i;
 	}
 
-	//找分數最大的
+	// 找分數最大的
 	public String getMaxScoreStudent() {
 
-		int subject = this.selectObject();
-
+		int subject = this.selectSubject();
 		int max = this.getStudentArray()[0][subject];
-		// System.out.println(max);
 		int order = 0;
+		
 		for (var i = 0; i <= this.getStudentCount(); i++) {
-			System.out.println(this.getStudentArray()[i][subject] + "  " + i + "  " + nameList[i]);
 			if (max < this.getStudentArray()[i][subject]) {
 				max = this.getStudentArray()[i][subject];
 				order = i;
@@ -163,40 +178,40 @@ public class ScoreCalculator {
 		System.out.println(order);
 		String maxPerson = nameList[order];
 
-		return maxPerson;
+		return "分數最高的是 :" + maxPerson;
 	}
 
-	//找最分數最高的
+	// 找最分數最高的
 	public String getMinScoreStudent() {
-		int subject = this.selectObject();
-
+		
+		int subject = this.selectSubject();
 		int min = this.getStudentArray()[0][subject];
-
 		int order = 0;
+		
 		for (var i = 0; i <= this.getStudentCount(); i++) {
-			System.out.println(this.getStudentArray()[i][subject] + "  " + i + "  " + nameList[i]);
 			if (min > this.getStudentArray()[i][subject]) {
 				min = this.getStudentArray()[i][subject];
 				order = i;
 			}
 		}
-		System.out.println(order);
 		String minPerson = nameList[order];
 
-		return minPerson;
+		return "分數最高的是 :" + minPerson;
 	}
 
-	//找平均
-	public double getAvgScore() {
-		double avg = 0;
-		int sum = 0;
-		int subject = this.selectObject();
+	// 找平均
+	public String getAvgScore() {
+		
+		String avg = "";
+		double sum = 0;
+		int subject = this.selectSubject();
+		
 		for (var i = 0; i < this.studentLimit; i++) {
 			sum += this.getStudentArray()[i][subject];
 		}
-		avg = sum / this.getStudentCount();
+		avg = Double.toString((sum / this.getStudentCount()));
 
-		return avg;
+		return "平均成績是 : " + avg;
 	}
 
 }
