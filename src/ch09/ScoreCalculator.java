c3ceb1f3bf3d63e528e317f11026b3387db0d84f
Bo-Xuan Fan <coffee0127@gmail.com>
2022-05-19 00:52:09 +0800

    Replace anonymous class with Lambda

--- src/ch09/ScoreCalculator.java
+++ src/ch09/ScoreCalculator.java
@@ -182,16 +182,13 @@ public class ScoreCalculator {
 
 	public String[] getTop3Student() {
 
-        Comparator<Student> comparator = new Comparator<>() {
-            @Override
-            public int compare(Student s1, Student s2) {
-                if (s1 != null && s2 != null) {
-                    int s1TotalGrade = s1.getChi() + s1.getEng() + s1.getMath();
-                    int s2TotalGrade = s2.getChi() + s2.getEng() + s2.getMath();
-                    return -(s1TotalGrade - s2TotalGrade);
-                } else {
-                    return 1;
-                }
+        Comparator<Student> comparator = (Student s1, Student s2) -> {
+            if (s1 != null && s2 != null) {
+                int s1TotalGrade = s1.getChi() + s1.getEng() + s1.getMath();
+                int s2TotalGrade = s2.getChi() + s2.getEng() + s2.getMath();
+                return -(s1TotalGrade - s2TotalGrade);
+            } else {
+                return 1;
             }
         };
         Arrays.sort(this.studentArray, comparator);
