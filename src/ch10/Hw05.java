d382798c508e143d4cd74ece222093831efbfdb2
Sisol Shie <sisolshie@gmail.com>
2022-05-26 10:18:54 +0800

    fix: wrong ans.
    It's "File" type not "String" to find the file size from length()

--- src/ch10/Hw05.java
+++ src/ch10/Hw05.java
@@ -1,5 +1,7 @@
 package ch10;
 
+import java.io.File;
+
 /**
  * 5.續第4題，印出sample1.txt、sample2.txt的檔案大小
  * 
@@ -8,11 +10,12 @@ package ch10;
 public class Hw05 {
 
 	public static void main(String[] args) {
-		String src1 = "file\\sample1.txt";
-		String src2 = "file\\sample2.txt";
 
-		System.out.println("sample1.txt的檔案大小是 : " + src1.length() + " byte");
-		System.out.println("sample2.txt的檔案大小是 : " + src2.length() + " byte");
+		File file1 = new File("file\\sample1.txt");
+		File file2 = new File("file\\sample2.txt");
+
+		System.out.println("sample1.txt的檔案大小是 : " + file1.length() + " byte");
+		System.out.println("sample2.txt的檔案大小是 : " + file2.length() + " byte");
 
 	}
 
